9de3fc5ff398db8763f94c9597c95368639dfc69
Ali Alagha <43938315+AliAlagha@users.noreply.github.com>
2022-05-12 11:04:20 +0300

    Add pagination lecture

--- Lectures/23 - Full Database (Pagination)/includes/system_constants.php
+++ Lectures/23 - Full Database (Pagination)/includes/system_constants.php
@@ -4,6 +4,6 @@
 Here the system constants are stored in a separate file and this file is included in the rest of the files when it is needed to access the system constants, in order to reduce code repetition and ease the modification of the constants in a single file.
 */
 
-$system = ['storage_base_url' => 'http://localhost:84/web_2022/Lectures/22%20-%20Database%20(2)/'];
+$system = ['storage_base_url' => 'http://localhost:84/23%20-%20Full%20Database%20(Pagination)/'];
 
 ?>
